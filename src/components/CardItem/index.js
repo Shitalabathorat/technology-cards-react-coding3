cdd74ae5eed50a6b9631c58e80316f5ba883ee68
Shitalabathorat <126850348+Shitalabathorat@users.noreply.github.com>
2023-03-28 15:20:44 +0530

    Update index.js

--- src/components/CardItem/index.js
+++ src/components/CardItem/index.js
@@ -1,18 +1,21 @@
+import {Component} from 'react'
 import '.index.css'
 
-const UserProfile = props => {
-  const {userDetails} = props
-  const {imgUrl, className, description, title, key} = userDetails
-}
-return (
-  <li className="user-card-container">
-    <div className={className}>
-      <h2 className="heading">{title}</h2>
-      <p className="para">{description}</p>
-      <div className="img-container">
-        <img src={imgUrl} className="avtar" alt="avtar" />
+const CardItem = props => {
+  const {userDetails, key} = props
+  const {imgUrl, className, description, title} = userDetails
+
+  return (
+    <li className={className}>
+      <div className="container">
+        <h2 className="heading">{title}</h2>
+        <p className="para">{description}</p>
+        <div className="img-container">
+          <img src={imgUrl} className="avtar" alt="avtar" />
+        </div>
       </div>
-    </div>
-  </li>
-)
-export default UserProfile
+    </li>
+  )
+}
+
+export default CardItem
